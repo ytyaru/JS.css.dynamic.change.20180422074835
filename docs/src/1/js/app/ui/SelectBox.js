1a33c2f0a3e0688dd977c16d82bc38b1c6985451
ytyaru <ytyaru@outlook.jp>
2018-04-22 10:15:24 +0900

    整理

--- docs/src/1/js/app/ui/SelectBox.js
+++ docs/src/1/js/app/ui/SelectBox.js
@@ -4,30 +4,23 @@ define(function(require, exports, module) {
 	static get Id() { return 'CssSelectBox'; }
 	static get LinkId() { return 'Style'; }
 	static AppendTo(parent) {
-	    console.log(parent);
-	    const dom = SelectBox.Create();
+	    const dom = SelectBox._Create();
 	    parent.appendChild(dom);
-	    console.log(document.body.querySelectorAll('select'));
 	    SelectBox._SetEvent(dom);
+	    dom.focus();
 	}
-	static Create() {
+	static _Create() {
 	    const html = SelectBox._MakeHtmlString();
 	    console.log(html);
 	    const parser = new DOMParser();
-	    //const dom = parser.parseFromString(html, "application/xml");
 	    const dom = parser.parseFromString(html, "text/html");
 	    console.log(dom);
-	    //if (0 < dom.body.querySelectorAll('parsererror').length) {
 	    if (0 < dom.querySelectorAll('parsererror').length) {
 		throw new Error('パースエラー', html);
 	    }
 	    const domSelect = dom.getElementsByTagName('select')[0];
 	    console.log(domSelect);
-	    //SelectBox._SetEvent(domSelect);
 	    return domSelect ;
-	    //SelectBox._SetEvent(dom);
-	    //return dom;
-	    //return dom.getElementsByTagName('select')[0];
 	}
 	static _MakeHtmlString() {
 	    const cssFiles = [
@@ -41,46 +34,12 @@ define(function(require, exports, module) {
 	    return XmlGen.Element('select', {'id': SelectBox.Id}, options.join(''));
 	}
 	static _CssPath(name) { return `./css/${name}.css`; }
+	// イベントの設定はdocumentにappendしてからでないと動かない
 	static _SetEvent(dom) {
-	    console.log(dom);
-	    //dom.onchange = e => {
 	    dom.onchange = e => {
 		console.log(e.target.value);
 		document.getElementById(SelectBox.LinkId).href = e.target.value;
 	    };
-	    /*
-	    */
-	    /*
-	    dom.onchange = () => {
-		console.log(this);
-		console.log(this.value);
-		document.getElementById(SelectBox.LinkId).href = this.value;
-		//console.log(e.target.value);
-		//document.getElementById(SelectBox.LinkId).href = e.target.value;
-	    };
-	    */
-	    /*
-	    dom.addEventListener('change', () => {
-		console.log(this);
-		console.log(this.value);
-		document.getElementById(SelectBox.LinkId).href = this.value;
-	    });
-	    */
-	    /*
-	    let elm = document.getElementById(SelectBox.Id);
-	    console.log(elm);
-	    elm.addEventListener('change', () => {
-		console.log(this);
-		console.log(this.value);
-		document.getElementById(SelectBox.LinkId).href = this.value;
-	    });
-	    */
-	    /*
-	    document.getElementById(SelectBox.Id).onchange = e => {
-		console.log(e.target.value);
-		document.getElementById(SelectBox.LinkId).href = e.target.value;
-	    };
-	    */
 	}
     };
 });
